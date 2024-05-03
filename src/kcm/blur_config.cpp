e0c2b9d33d9befd846d5173f8c7cc3c5292cb07f
taj-ny <79316397+taj-ny@users.noreply.github.com>
2024-05-03 17:12:10 +0200

    kcm: populate table with current rules

--- src/kcm/blur_config.cpp
+++ src/kcm/blur_config.cpp
@@ -4,12 +4,15 @@
     SPDX-License-Identifier: GPL-2.0-or-later
 */
 #include "blur_config.h"
+#include "config/config.h"
 
 //#include <config-kwin.h>
 
 // KConfigSkeleton
 
 #include <KPluginFactory>
+#include <QCheckBox>
+#include <QLabel>
 #include "kwineffects_interface.h"
 
 namespace KWin
@@ -21,8 +24,20 @@ BlurEffectConfig::BlurEffectConfig(QObject *parent, const KPluginMetaData &data)
     : KCModule(parent, data)
 {
     ui.setupUi(widget());
+    BlurConfig::instance()->read();
 
     ui.rulesTable->horizontalHeader()->setStretchLastSection(true);
+    ui.rulesTable->setRowCount(BlurConfig::instance()->rules()->rules.size());
+    int row = 0;
+
+    for (const auto &rule : BlurConfig::instance()->rules()->rules) {
+        auto checkBox = new QCheckBox();
+        checkBox->setChecked(rule->enabled);
+
+        ui.rulesTable->setCellWidget(row, 0, checkBox);
+        ui.rulesTable->setCellWidget(row, 1, new QLabel(rule->name));
+        row++;
+    }
 }
 
 BlurEffectConfig::~BlurEffectConfig()
